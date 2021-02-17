26219ba04989c77cfcd5d3a6761172f9cf5e3cd4
¨Oscar Wyatt¨ <mail@oscarwyatt.co.uk>
2021-02-17 11:23:17 +0000

    Adapt to use with search-autocomplete-api It is hosted on PaaS and needs to be added to CSP

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,1 +1,5 @@
 GovukContentSecurityPolicy.configure
+
+Rails.application.config.content_security_policy do |p|
+  p.connect_src(*Rails.application.config.content_security_policy.connect_src, "http://search-autocomplete-api.london.cloudapps.digital/")
+end
